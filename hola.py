260c4344cfefeea0a32544379d75dc1d7d7fa9bd
zramirezg <zramirezg@unemi.edu.ec>
2026-09-04 21:10:31 -0500

    soy ess

--- hola.py
+++ hola.py
@@ -1,1 +1,11 @@
 print('Hola')
+
+class Person:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def hablar(self):
+        return 'Cinthia te amo'
+
+persona1 = Person('Zaul')
+persona1.hablar
