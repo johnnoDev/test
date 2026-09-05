ec2d869a6ce2e8ca5112a91da9a4d2ad9f64514c
zramirezg <zramirezg@unemi.edu.ec>
2026-09-04 22:05:02 -0500

    migajas miu

--- caicedo.py
+++ caicedo.py
@@ -1,3 +1,16 @@
 print('Caicedo')
 
 print("adhdjbajdads")
+
+
+class Persona:
+    def __init__(self, nombre, edad, pais):
+        self.nombre = nombre
+        self.edad = edad
+        self.pais = pais
+
+    def migajiar(self, nombre):
+        return f'{nombre} dame migajas'
+
+caiza = Persona('caiza', 19, 'Ecuador')
+print(caiza.migajiar("abbii"))
