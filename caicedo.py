8b5efac9144dce97a01c0444fbee77f23aa6bcaa
johnnoDev <johnny.lopezvera20@gmail.com>
2026-09-04 22:05:11 -0500

    hay mi caicedo

--- caicedo.py
+++ caicedo.py
@@ -1,1 +1,18 @@
 print('Caicedo')
+
+class Caicedo:
+    def __init__(self, nombre, apellido, migajero=True):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.migajero = migajero
+
+    def migajear(self, persona):
+            return f'Estoy migajeando a.... {persona}'
+
+    def __str__(self):
+        return f'{self.nombre} {self.apellido} es migajero: {self.migajero}'
+
+
+caicedo = Caicedo('Jonnathan', 'Caiza')
+print(caicedo.migajear('Abbi'))
+print(caicedo)
